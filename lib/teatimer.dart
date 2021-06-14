d764a51f64a3ccedeb993654368f74076e78a24b
Christian Anthony <christiananthony1981@gmail.com>
2021-06-14 17:17:03 -0400

    Cleaned up TeaTimer logic
    Timer functions as intended.
    Added isRunning bool for easier interfacing for other functions.

--- lib/teatimer.dart
+++ lib/teatimer.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-
 class TeaTimer extends StatefulWidget {
   @override 
   _TeaTimerState createState() => _TeaTimerState();
@@ -11,6 +10,7 @@ class _TeaTimerState extends State<TeaTimer>{
   
   Timer? _timer;
   Duration _currentDuration = Duration();
+  bool isRunning = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,41 +27,61 @@ class _TeaTimerState extends State<TeaTimer>{
     );
   }
 
-
-
-
-
-
-
-  //Creates a new periodic timer
+  //Creates a new periodic timer. 
   void startTimer()
   {
+    //This is to check if theres a currently running timer but i dont think that could ever be when calling start.
+    if (_timer != null){
+      _timer?.cancel();
+    }
 
-    
-
-
-
-
-
-
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) { onTick(); });
+    isRunning = true;
   }
 
+  //Stops the timer currently running
+  void stopTimer()
+  {
+    _timer?.cancel();
+    isRunning = false;
+  }
 
+  //Logic for the timer to call every tick
+  void onTick()
+  {
+    if (_currentDuration == Duration.zero)
+    {
+      stopTimer();
+      soundAlarm();
+    }
+    else
+    {
+      decrementSeconds();
+    }
+  }
 
+  //Decrements the time display
+  void decrementSeconds()
+  {
+    setState(() 
+    {
+      _currentDuration -= Duration(seconds: 1); 
+    });
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  //Clears any currently running timers and creates new timer
+  void onButtonPress()
+  {
+    if (isRunning){
+      stopTimer();
+    }
+    else{
+      setState(() {
+        _currentDuration = Duration(seconds: 10);   //This is a temp hardcoded value for testing
+        startTimer();
+      });
+    }
+  }
 
   //Displays the formatted text for the timer duration
   Widget timerText(BuildContext context){
@@ -71,67 +91,8 @@ class _TeaTimerState extends State<TeaTimer>{
     ),);
   }
 
-
-
-  //Stops the timer currently running
-  void stopTimer()
-  {
-    _timer?.cancel();
-  }
-
-  //Whoops, I made a stopwatch
-  void incrementSeconds()
-  {
-    setState(() {
-      _currentDuration += Duration(seconds: 1);
-    });
-  }
-
-  //Decrements the time display
-  void decrementSeconds()
-  {
-    if (_timer?.tick == _currentDuration.inSeconds)
-    {
-      soundAlarm();
-      stopTimer();
-    }
-    else
-    {
-      setState(() 
-      {
-        _currentDuration -= Duration(seconds: 1); 
-      });
-    }
-  }
-
-  void soundAlarm(){
-
-  }
-
-  //Clears any currently running timers and creates new timer
-  void onButtonPress(){
-
-    if (_timer != null && _timer!.isActive)
-    {
-      stopTimer();
-    }
-    else{
-      setState(() {
-        _currentDuration = Duration.zero;
-        startTimer();
-      });
-    }
-  }
-
-
-
-
-
-
-
-
-
-
+  //Called on timer completion, plays sound
+  void soundAlarm(){}
 }
 
  
